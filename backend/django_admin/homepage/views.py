828418430462d8d86278fd9dd3f1b4da13132106
Suryansh Sharma <suryansharma09@gmail.com>
2025-03-04 22:59:02 +0530

    homepage modified to include sample data

--- backend/django_admin/homepage/views.py
+++ backend/django_admin/homepage/views.py
@@ -1,9 +1,34 @@
-
-
-# Create your views here.
-from rest_framework.response import Response
+from django.shortcuts import get_list_or_404
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Destination  # To be created later
+
 
 @api_view(['GET'])
 def homepage_view(request):
-    return Response({"message": "Welcome to Travel App Curator!"})
+    destinations = get_list_or_404(Destination.objects.all())  # Fetch from DB
+
+    data = {
+        "hero_section": {
+            "search_placeholder": "Search for a country...",
+            "cta_text": "Get Started"
+        },
+        "how_it_works": [
+            "Choose a country",
+            "Select the best travel apps",
+            "Generate your QR code"
+        ],
+        "popular_destinations": [
+            {"name": d.name, "image": d.image_url} for d in destinations
+        ],
+        "footer": {
+            "about": "About Travel App Curator",
+            "contact": "Contact Us",
+            "privacy": "Privacy Policy",
+            "social_links": [
+                {"platform": "Twitter", "url": "https://twitter.com"},
+                {"platform": "Facebook", "url": "https://facebook.com"}
+            ]
+        }
+    }
+    return Response(data)
