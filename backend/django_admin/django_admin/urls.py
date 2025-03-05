426c6b357a43cd34562a8c697ca510269dc0daa6
Suryansh Sharma <suryansharma09@gmail.com>
2025-03-05 21:55:08 +0530

    created country app

--- backend/django_admin/django_admin/urls.py
+++ backend/django_admin/django_admin/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/homepage/', include('homepage.urls')),
+    path('api/country/', include('country.urls')),
 ]
