09606d07d5ec44902981b0f7c40e3fcead2073b1
Suryansh Sharma <suryansharma09@gmail.com>
2025-06-23 13:38:16 +0530

    added site

--- backend/django_admin/django_admin/settings.py
+++ backend/django_admin/django_admin/settings.py
@@ -73,11 +73,11 @@ INSTALLED_APPS = [
 ]
 
 
-SITE_ID = 1
+SITE_ID = 2
 
 REST_USE_JWT = True        
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-ACCOUNT_AUTHENTICATION_METHOD = "username_email"
+ACCOUNT_LOGIN_METHODS = {'username', 'email'}
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = True
@@ -156,18 +156,18 @@ WSGI_APPLICATION = 'django_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER",),
-#         'PASSWORD': os.getenv("DB_PASSWORD",),
-#         'HOST': os.getenv("DB_HOST", ),
-#         'PORT': os.getenv("DB_PORT", ),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER",),
+        'PASSWORD': os.getenv("DB_PASSWORD",),
+        'HOST': os.getenv("DB_HOST", ),
+        'PORT': os.getenv("DB_PORT", ),
         
-#     },
+    },
  
-# }
+}
 
 # DATABASES = {
 #     'default': dj_database_url.parse(
