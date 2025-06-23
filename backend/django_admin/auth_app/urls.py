efc181fd0f70ebc02e83ff5a1044bc5c23d50554
Suryansh Sharma <suryansharma09@gmail.com>
2025-06-23 21:34:02 +0530

    added user detials

--- backend/django_admin/auth_app/urls.py
+++ backend/django_admin/auth_app/urls.py
@@ -2,6 +2,7 @@
 
 from django.urls import path, include
 from .views import GoogleLogin, FacebookLogin
+from dj_rest_auth.views import UserDetailsView
 
 urlpatterns = [
     # username/password login, logout, password reset, etc.
@@ -10,7 +11,7 @@ urlpatterns = [
     # registration (sign-up) + email verification
     path("registration/", include("dj_rest_auth.registration.urls")),
 
-  
+     path("user/", UserDetailsView.as_view(), name="rest_user_details"),
       # social-login endpoints
     path("social/google/", GoogleLogin.as_view(), name="google_login"),
     path("social/facebook/", FacebookLogin.as_view(), name="facebook_login"),
