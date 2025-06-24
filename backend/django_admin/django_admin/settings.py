27d42288a6ec7398ca782cba196cd6cf4c76a01c
Suryansh Sharma <suryansharma09@gmail.com>
2025-06-24 14:59:39 +0530

    fixed auth

--- backend/django_admin/django_admin/settings.py
+++ backend/django_admin/django_admin/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 
 
 #django will load .env file at the beginning 
@@ -156,25 +156,25 @@ WSGI_APPLICATION = 'django_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv("DB_NAME"),
-#         'USER': os.getenv("DB_USER",),
-#         'PASSWORD': os.getenv("DB_PASSWORD",),
-#         'HOST': os.getenv("DB_HOST", ),
-#         'PORT': os.getenv("DB_PORT", ),
-        
-#     },
- 
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv("DATABASE_URL"),
-        conn_max_age=600 # Optional: Reconnects to the database every 10 minutes to prevent stale connections
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER",),
+        'PASSWORD': os.getenv("DB_PASSWORD",),
+        'HOST': os.getenv("DB_HOST", ),
+        'PORT': os.getenv("DB_PORT", ),
+        
+    },
+ 
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(
+#         os.getenv("DATABASE_URL"),
+#         conn_max_age=600 # Optional: Reconnects to the database every 10 minutes to prevent stale connections
+#     )
+# }
 
 # # Redis Configuration
 # REDIS_HOST = os.getenv("REDIS_HOST")
